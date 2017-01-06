2bf6b9167eb99631518359c3c3bab246bda1e1c4
Commrade <dyloneal@gmail.com>
2017-01-06 17:45:56 -0500

    add task class

--- todo.rb
+++ todo.rb
@@ -1,6 +1,21 @@
 # Classes
 
 class List
+	attr_reader :all_tasks
+	def initialize
+		@all_tasks = []
+	end
+
+	def add(task)
+		all_tasks << task
+	end
+end
+
+class Task
+	attr_reader :description
+	def initialize(description)
+		@description = description
+	end
 end
 
 # actions
@@ -8,4 +23,6 @@ end
 if __FILE__ == $PROGRAM_NAME
 	my_list = List.new
 	puts 'You have created a new list'
+	my_list.add(Task.new('test'))
+	puts 'You have added a task to the Todo List'
 end
